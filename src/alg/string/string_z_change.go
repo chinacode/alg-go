dbeab42834de6b45ee1db81bd42290dd4dfb1518
Martin Lee <2548252929@qq.com>
2020-09-10 18:02:06 +0800

    array get three sum

--- src/alg/string/string_z_change.go
+++ src/alg/string/string_z_change.go
@@ -2,26 +2,85 @@ package string
 
 import (
 	"fmt"
+	"strings"
 	"util"
 )
 
-func getStringZChange2(nums []int, target int) []int {
-	result := []int{}
-	m := make(map[int]int)
-	for i, k := range nums {
-		if value, exist := m[target-k]; exist {
-			result = append(result, value)
-			result = append(result, i)
-		}
-		m[k] = i
+/**
+ * N 变化 先找规律 每一行 都是按照规则进行取模
+ */
+func getStringZChange2(tmpStr string, rowNum int) string {
+	if rowNum == 1 {
+		return tmpStr
 	}
-	return result
+	var b = []rune(tmpStr)
+	var res = make([]string, rowNum)
+	var length = len(b)
+	var period = rowNum*2 - 2
+	for i := 0; i < length; i++ {
+		var mod = i % period
+		var str = string(b[i])
+		if mod < rowNum {
+			res[mod] += str
+		} else {
+			res[period-mod] += str
+		}
+	}
+	return strings.Join(res, "")
 }
 
-func getStringZChange(tmpStr string, lineNum int) string {
-	newStr := ""
+func getStringZChange2Arr(tmpStr string, rowNum int) [][]string {
+	resultArr := make([][]string, rowNum)
+	if rowNum == 1 {
+		return resultArr
+	}
+	var length = len(tmpStr)
+	var period = rowNum*2 - 2
+	for i := 0; i < length; i++ {
+		var mod = i % period
+		var str = tmpStr[i : i+1]
+		var index = period - mod
+		if mod < rowNum {
+			index = mod
+		}
+		resultArr[index] = append(resultArr[index], str)
+	}
+	return resultArr
+}
 
-	return newStr
+func getStringZChangeArr(tmpStr string, rowNum int) [][]string {
+	resultArr := [][]string{}
+	if rowNum == 1 {
+		return resultArr
+	}
+	stringLen := len(tmpStr)
+
+	column := 0
+	tmpIndex := rowNum
+	period := rowNum - 1
+	columnArr := []string{}
+	for i := 0; i < stringLen; i++ {
+		selStr := tmpStr[i : i+1]
+		if column == 0 || column%period == 0 {
+			columnArr = append(columnArr, selStr)
+		} else {
+			columnArr = make([]string, rowNum)
+			columnArr[tmpIndex-1] = selStr
+		}
+		if len(columnArr) == rowNum {
+			resultArr = append(resultArr, columnArr)
+			columnArr = []string{}
+			column++
+			tmpIndex--
+			if tmpIndex <= 1 {
+				tmpIndex = rowNum
+			}
+		}
+	}
+
+	//fmt.Println(resultArr)
+	//return newStr
+	return resultArr
 }
 
 /**
@@ -31,16 +90,17 @@ E C   I H   N
 T     S     G
 */
 func TestGetStringZChange() {
-	tmpStr := "LEETCODEISHIRING"
+	tmpStr := "LEETCODEISHIRINGALSJDLASJDLASJLDJSLDJSA"
 
 	var resultData string
+	var resultDataArr [][]string
 	//ThreeSum := 0
-	lineNum := 4
+	rowNum := 8
 	loopCount := 1
-	loopCount = 3000000
+	loopCount = 200000
 	util.Start("first", "")
 	for i := 0; i < loopCount; i++ {
-		resultData = getStringZChange(tmpStr, lineNum)
+		resultData = getStringZChange2(tmpStr, rowNum)
 	}
 	fmt.Println(resultData)
 	util.Cut("first", "")
@@ -51,5 +111,19 @@ func TestGetStringZChange() {
 	}
 	fmt.Println(resultData)
 	util.Cut("second", "")
+
+	util.Start("first arr", "")
+	for i := 0; i < loopCount; i++ {
+		resultDataArr = getStringZChangeArr(tmpStr, rowNum)
+	}
+	fmt.Println(resultDataArr)
+	util.Cut("first arr", "")
+
+	util.Start("second arr", "")
+	for i := 0; i < loopCount; i++ {
+		resultDataArr = getStringZChange2Arr(tmpStr, rowNum)
+	}
+	fmt.Println(resultDataArr)
+	util.Cut("second arr", "")
 
 }
