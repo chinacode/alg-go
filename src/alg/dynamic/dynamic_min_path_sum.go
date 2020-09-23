eae95af404da83feead58dc10d057d8cc45d2f37
Martin Lee <2548252929@qq.com>
2020-09-23 20:06:25 +0800

    dynamic array path min sum

--- src/alg/dynamic/dynamic_min_path_sum.go
+++ src/alg/dynamic/dynamic_min_path_sum.go
@@ -5,23 +5,28 @@ import (
 	"util"
 )
 
-func MinPathSum2(triangle [][]int) int {
-	if len(triangle) < 1 {
+func MinPathSum2(grid [][]int) int {
+	l := len(grid)
+	if l < 1 {
 		return 0
 	}
-	if len(triangle) == 1 {
-		return triangle[0][0]
-	}
-	dp := make([][]int, len(triangle))
-	for i, arr := range triangle {
+	dp := make([][]int, l)
+	for i, arr := range grid {
 		dp[i] = make([]int, len(arr))
 	}
-	result := 1<<31 - 1
-	//fmt.Println(dp)
-	for _, k := range dp[len(dp)-1] {
-		result = util.Min(result, k)
+	dp[0][0] = grid[0][0]
+	for i := 0; i < l; i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if i == 0 && j != 0 {
+				dp[i][j] = dp[i][j-1] + grid[i][j]
+			} else if j == 0 && i != 0 {
+				dp[i][j] = dp[i-1][j] + grid[i][j]
+			} else if i != 0 && j != 0 {
+				dp[i][j] = util.Min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+			}
+		}
 	}
-	return result
+	return dp[l-1][len(dp[l-1])-1]
 }
 
 func MinPathSum(longArr [][]int) int {
@@ -36,13 +41,30 @@ func MinPathSum(longArr [][]int) int {
 	}
 
 	//fmt.Println(dp)
-	sum := 1<<31 - 1
-	for _, v := range dp[level-1] {
-		if v < sum {
-			sum = v
+	for i := 0; i < len(longArr[0]); i++ {
+		if i == 0 {
+			dp[0][0] = longArr[0][0]
+		} else {
+			dp[0][i] = dp[0][i-1] + longArr[0][i]
 		}
 	}
-	return sum
+
+	for l := 1; l < level; l++ {
+		levelLen := len(longArr[l])
+		for i := 0; i < levelLen; i++ {
+			//fmt.Println(l, i)
+			if i == 0 {
+				dp[l][i] = util.Min(dp[l-1][i], dp[l-1][len(dp[l-1])-1])
+			} else {
+				dp[l][i] = util.Min(dp[l-1][i], dp[l][i-1])
+			}
+
+			dp[l][i] = dp[l][i] + longArr[l][i]
+		}
+	}
+	//util.PrintLevelArray(dp)
+
+	return dp[level-1][len(dp[level-1])-1]
 }
 
 /**
@@ -59,12 +81,12 @@ func TestMinPathSum() {
 	输出: 7
 	解释: 因为路径 1→3→1→1→1 的总和最小。
 	*/
-	longArr := util.InitRandLevelArrayRange(3, 3, 1, 10)
+	longArr := util.InitRandLevelArrayRange(3, 4, 1, 10)
 	util.PrintLevelArray(longArr)
 
 	var resultData int
 	loopCount := 1
-	//loopCount = 5000000
+	loopCount = 5000000
 	util.Start("first", "")
 	for i := 0; i < loopCount; i++ {
 		resultData = MinPathSum(longArr)
