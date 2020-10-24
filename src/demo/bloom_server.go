de011c5651942e1abbb40e50772c52b7b854ecba
Martin Lee <2548252929@qq.com>
2020-10-25 01:02:13 +0800

    http server for bloom filter return status

--- src/demo/bloom_server.go
+++ src/demo/bloom_server.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"github.com/steakknife/bloomfilter"
 	"hash/fnv"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"reflect"
 	"strconv"
 	"strings"
 )
@@ -28,17 +30,19 @@ var (
 )
 
 type Result struct {
-	code int8
-	msg  string
-	data string
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
 }
 
 func Main() {
 	SPLIT = rune(',')
+
 	http.HandleFunc("/config", Config)
 	http.HandleFunc("/add", Add)
 	http.HandleFunc("/batch_add", BatchAdd)
 	http.HandleFunc("/exists", Exists)
+	http.HandleFunc("/batch_exists", BatchExists)
 	http.HandleFunc("/clear", Clear)
 	http.HandleFunc("/save", Save)
 	http.HandleFunc("/load", Load)
@@ -49,13 +53,41 @@ func Main() {
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(SERVER_PORT), nil))
 }
 
-func getErrorResult(msg string) Result {
-	return Result{code: http.StatusInternalServerError, msg: msg, data: nil}
+func getErrorResult(msg string) *Result {
+	result := Result{Code: http.StatusInternalServerError, Msg: msg, Data: ""}
+	return &result
 }
 
-func getResult(data map[string]string) Result {
-	jsonData, _ := json.Marshal(data)
-	return Result{code: http.StatusOK, msg: "success", data: string(jsonData)}
+func getResult(data interface{}) *Result {
+	var result Result
+	if reflect.TypeOf(data).Name() != "string" {
+		jsonData, _ := json.Marshal(data)
+		result = Result{Code: http.StatusOK, Msg: "success", Data: string(jsonData)}
+	} else {
+		result = Result{Code: http.StatusOK, Msg: "success", Data: fmt.Sprint(data)}
+	}
+	return &result
+}
+
+func responseSuccess(response http.ResponseWriter, data interface{}) {
+	result := getResult(data)
+	json, err := json.Marshal(&result)
+	if nil != err {
+		log.Fatal(err)
+	}
+	response.WriteHeader(result.Code)
+	response.Write(json)
+
+	log.Printf("<<<response %s", json)
+}
+
+func responseError(response http.ResponseWriter, msg string) {
+	result := getErrorResult(msg)
+	json, _ := json.Marshal(&result)
+	response.WriteHeader(result.Code)
+	response.Write(json)
+
+	log.Printf("<<<response %s", json)
 }
 
 func initParams(response http.ResponseWriter, request *http.Request) (bool, map[string]bool) {
@@ -77,7 +109,12 @@ func initParams(response http.ResponseWriter, request *http.Request) (bool, map[
 	}
 
 	paramKey = "keys"
-	if nil != query[paramKey] {
+	bodyData := ""
+	if nil != request.Body {
+		bodyData, _ := ioutil.ReadAll(request.Body)
+		keys = string(bodyData)
+		params[paramKey] = true
+	} else if nil != query[paramKey] || nil != request.Body {
 		keys = query[paramKey][0]
 		params[paramKey] = true
 	} else {
@@ -108,23 +145,64 @@ func initParams(response http.ResponseWriter, request *http.Request) (bool, map[
 		params[paramKey] = false
 	}
 
+	log.Printf(">>>request url %s , body %s", request.RequestURI, bodyData)
+	response.Header().Add("server", "bloom server")
 	if "" == bucket || len(bucket) <= 0 {
-		response.WriteHeader(http.StatusBadRequest)
-		response.Write([]byte(fmt.Sprintf("params bucket must set!")))
+		responseError(response, fmt.Sprintf("params bucket must set!"))
 		return false, params
 	}
 	if bloomfilterMap[bucket] == nil && !strings.Contains(request.RequestURI, "config") {
-		response.WriteHeader(http.StatusBadRequest)
-		response.Write([]byte(fmt.Sprintf("bucket %s not exists, please use /config init first", bucket)))
+		responseError(response, fmt.Sprintf("bucket %s not exists, please use /config init first", bucket))
 		return false, params
 	}
 	return true, params
 }
 
-func initBucket() {
-	if len(bucket) > 0 {
-		bloomfilterMap[bucket], err = bloomfilter.NewOptimal(elements, percent)
+func Config(response http.ResponseWriter, request *http.Request) {
+	check, params := initParams(response, request)
+	if !check {
+		return
 	}
+	if bloomfilterMap[bucket] != nil {
+		responseError(response, fmt.Sprintf("bucket %s exists, please reset first", bucket))
+		return
+	}
+	if !params["percent"] || !params["elements"] {
+		responseError(response, fmt.Sprintf("percent(%f) and elements(%d) must set!", percent, elements))
+		return
+	}
+	bloomfilterMap[bucket], err = bloomfilter.NewOptimal(elements, percent)
+	responseSuccess(response, fmt.Sprintf("success"))
+}
+
+func Load(response http.ResponseWriter, request *http.Request) {
+	check, _ := initParams(response, request)
+	if !check {
+		return
+	}
+	filename := fmt.Sprintf("BF.%s.bucket", bucket)
+	r, err := os.Open(filename)
+	if err != nil {
+		responseError(response, fmt.Sprintf("back file %s not exists", filename))
+		return
+	}
+	defer func() {
+		err = r.Close()
+	}()
+
+	bloomfilterMap[bucket].ReadFrom(r)
+	responseSuccess(response, fmt.Sprintf("load from file %s success", filename))
+}
+
+func Save(response http.ResponseWriter, request *http.Request) {
+	check, _ := initParams(response, request)
+	if !check {
+		return
+	}
+
+	filename := fmt.Sprintf("BF.%s.bucket", bucket)
+	bloomfilterMap[bucket].WriteFile(filename)
+	responseSuccess(response, fmt.Sprintf("save to file %s success", filename))
 }
 
 func BatchAdd(response http.ResponseWriter, request *http.Request) {
@@ -133,8 +211,7 @@ func BatchAdd(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 	if !params["keys"] {
-		response.WriteHeader(http.StatusBadRequest)
-		response.Write([]byte(fmt.Sprintf("keys must set!")))
+		responseError(response, fmt.Sprintf("keys must set!"))
 		return
 	}
 
@@ -149,7 +226,7 @@ func BatchAdd(response http.ResponseWriter, request *http.Request) {
 		bloomfilterMap[bucket].Add(hash)
 		count++
 	}
-	response.Write([]byte(strconv.Itoa(count)))
+	responseSuccess(response, count)
 }
 
 func Add(response http.ResponseWriter, request *http.Request) {
@@ -158,15 +235,42 @@ func Add(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 	if !params["key"] {
-		response.WriteHeader(http.StatusBadRequest)
-		response.Write([]byte(fmt.Sprintf("key must set!")))
+		responseError(response, fmt.Sprintf("key must set!"))
 		return
 	}
 
 	hash := fnv.New64()
 	hash.Write([]byte(key))
 	bloomfilterMap[bucket].Add(hash)
-	response.Write([]byte("success"))
+	responseSuccess(response, "success")
+}
+
+func BatchExists(response http.ResponseWriter, request *http.Request) {
+	check, params := initParams(response, request)
+	if !check {
+		return
+	}
+	if !params["keys"] {
+		responseError(response, fmt.Sprintf("keys must set!"))
+		return
+	}
+
+	var batchList []int8
+	list := strings.FieldsFunc(keys, stringSpilt)
+	for _, keyString := range list {
+		if len(keyString) <= 0 {
+			continue
+		}
+		hash := fnv.New64()
+		hash.Write([]byte(keyString))
+		exists := bloomfilterMap[bucket].Contains(hash)
+		if exists {
+			batchList = append(batchList, 1)
+		} else {
+			batchList = append(batchList, 0)
+		}
+	}
+	responseSuccess(response, batchList)
 }
 
 func Exists(response http.ResponseWriter, request *http.Request) {
@@ -175,19 +279,20 @@ func Exists(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 	if !params["key"] {
-		response.WriteHeader(http.StatusBadRequest)
-		response.Write([]byte(fmt.Sprintf("key must set!")))
+		responseError(response, fmt.Sprintf("key must set!"))
 		return
 	}
 
+	ret := -1
 	hash := fnv.New64()
 	hash.Write([]byte(key))
 	exists := bloomfilterMap[bucket].Contains(hash)
 	if exists {
-		response.Write([]byte("1"))
+		ret = 1
 	} else {
-		response.Write([]byte("0"))
+		ret = 0
 	}
+	responseSuccess(response, ret)
 }
 
 func Clear(response http.ResponseWriter, request *http.Request) {
@@ -196,7 +301,7 @@ func Clear(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 	bloomfilterMap[bucket] = nil
-	response.Write([]byte("success"))
+	responseSuccess(response, "success")
 }
 
 func Memory(response http.ResponseWriter, request *http.Request) {
@@ -205,7 +310,8 @@ func Memory(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	response.Write([]byte(fmt.Sprintf("%d", bloomfilterMap[bucket].M())))
+	count := bloomfilterMap[bucket].M()
+	responseSuccess(response, fmt.Sprintf(" %d mb, %d kb , %d", count/1024/1024, count/1024, count))
 }
 
 func KeyCount(response http.ResponseWriter, request *http.Request) {
@@ -213,56 +319,5 @@ func KeyCount(response http.ResponseWriter, request *http.Request) {
 	if !check {
 		return
 	}
-	response.Write([]byte(fmt.Sprintf("%d", bloomfilterMap[bucket].K())))
-}
-
-func Config(response http.ResponseWriter, request *http.Request) {
-	check, params := initParams(response, request)
-	if !check {
-		return
-	}
-	if bloomfilterMap[bucket] != nil {
-		response.WriteHeader(http.StatusBadRequest)
-		response.Write([]byte(fmt.Sprintf("bucket %s exists, please reset first", bucket)))
-		return
-	}
-	if !params["percent"] || !params["elements"] {
-		response.WriteHeader(http.StatusBadRequest)
-		response.Write([]byte(fmt.Sprintf("percent(%f) and elements(%d) must set!", percent, elements)))
-		return
-	}
-	bloomfilterMap[bucket], err = bloomfilter.NewOptimal(elements, percent)
-	response.Write([]byte("success"))
-}
-
-func Load(response http.ResponseWriter, request *http.Request) {
-	check, _ := initParams(response, request)
-	if !check {
-		return
-	}
-	filename := fmt.Sprintf("BF.%s.bucket", bucket)
-
-	r, err := os.Open(filename)
-	if err != nil {
-		response.WriteHeader(http.StatusBadRequest)
-		response.Write([]byte(fmt.Sprintf("back file %s not exists", filename)))
-		return
-	}
-	defer func() {
-		err = r.Close()
-	}()
-
-	bloomfilterMap[bucket].ReadFrom(r)
-	response.Write([]byte(fmt.Sprintf("load from file %s success", filename)))
-}
-
-func Save(response http.ResponseWriter, request *http.Request) {
-	check, _ := initParams(response, request)
-	if !check {
-		return
-	}
-
-	filename := fmt.Sprintf("BF.%s.bucket", bucket)
-	bloomfilterMap[bucket].WriteFile(filename)
-	response.Write([]byte(fmt.Sprintf("save to file %s success", filename)))
+	responseSuccess(response, bloomfilterMap[bucket].K())
 }
