a7d342800c789da94a5b580e15ef1ba98cb64a84
Martin Lee <2548252929@qq.com>
2020-11-10 20:45:40 +0800

    shell

--- src/demo/sh/start.sh
+++ src/demo/sh/start.sh
@@ -2,4 +2,4 @@
 
 cd /data/bloom_server;
 
-nohup ./bloom_server -u edm -p 3306  -h rm-j6ct209xj16ef37jb90150.mysql.rds.aliyuncs.com -P EDM@20201110 -d edm_crawl &
+nohup ./bloom_server -u edm -p 3306  -h rm-j6ct209xj16ef37jb90150.mysql.rds.aliyuncs.com -P EDM@20201110 -d edm_crawl > /dev/null 2>&1 &
