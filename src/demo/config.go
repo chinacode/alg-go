f1f2a7f1a2940f71ae8f914b9a2ae68d71e775fd
Martin Lee <2548252929@qq.com>
2020-11-20 18:02:11 +0800

    fix import bugs

--- src/demo/config.go
+++ src/demo/config.go
@@ -18,8 +18,8 @@ type Config struct {
 
 var (
 	config = Config{
-		//bloom: BloomServer{host: "127.0.0.1", port: 9002}, //test local
-		bloom: BloomServer{host: "8.210.223.207", port: 9002}, // release online
+		bloom: BloomServer{host: "127.0.0.1", port: 9002}, //test local
+		//bloom: BloomServer{host: "8.210.223.207", port: 9002}, // release online
 		mysql: MysqlServer{host: "192.168.1.200", port: 3306, user: "root", password: "Paramida@2019", database: "edm_crawl"},
 	}
 )
